4a865ef68ca2572d641a69bcd2176fe86ea39189
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2019-10-13 22:22:48 +0200

    move

---
+++ WEBSITE-AGAT/txt/textes.js
@@ -0,0 +1,12 @@
+var apropos = "AGAT est né d'un constat simple : la multiplication des outils numériques de cartographie proposés aux collectivités, souvent complexes, ne parviennent pas toujours à combler les réels besoins métiers de leurs utilisateurs.<br>Combien de SIG, très coûteux, mis en place, et que personne ne sait utiliser en mairie ?<br>Face à ce qui reste un énorme besoin en matière de solutions de gestion de l’information géographique, le cabinet AGAT se positionne comme un acteur privilégié auprès des gestionnaires de territoires : partant de l'évaluation précise de leurs besoins avec une approche métier, les solutions AGAT intègrent des fonctionnalités simples et pertinentes pour les différents acteurs locaux.";
+
+var auditTxt = "Nous vous accompagnons pour la gestion optimisée de vos données géolocalisées à l'échelle de votre structure";
+var urbaTxt = "Dématérialisation de vos PLU(i) ou autres documents d'urbanisme, au format SIG respectant la norme INSPIRE, et téléversement sur le Géoportail de l'Urbanisme<br><br><a href='http://www.conformite-geoportail-urbanisme.fr/' target='_blank'>http://www.conformite-geoportail-urbanisme.fr/</a>";
+var formaTxt = "Formations à QGIS orientées métier pour les ingénieurs territoriaux, bureaux d'études, services techniques";
+var integrTxt = "Enrichissez votre SIG en intégrant les données métier de votre quotidien";
+var portailTxt = "Mettez en valeur votre territoire avec un portail web cartographique";
+var reglemTxt = "Dans le cadre du partenariat national Carto-réseaux, confiez-nous la numérisation de vos réseaux et leur téléversement sur le portail de l'INERIS <a href='http://www.carto-reseaux.fr' target='_blank'>http://www.carto-reseaux.fr</a>";
+
+
+//revenir à la ligne: <br>
+//insérer image: <img src="img/xxxx.png"> en mettant l'image dans le dossier img/
